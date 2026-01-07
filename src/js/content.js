2a7935848b63777b75d8ee796a2ef97afceae8df
UnnamedCoder <73340160+donotfind@users.noreply.github.com>
2026-01-07 11:38:13 +0530

    Update content.js

--- src/js/content.js
+++ src/js/content.js
@@ -63,7 +63,7 @@
     `
 
     const pageScript = document.createElement("script")
-    pageScript.src = chrome.runtime.getURL("src/page.js")
+    pageScript.src = chrome.runtime.getURL("src/js/page.js")
 
     pageScript.onload = () => {
       pageScript.remove()
